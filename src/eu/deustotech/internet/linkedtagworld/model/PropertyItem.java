c279d891e7b612796fd724ca7273c4681a157095
Jon Lazaro <lazaro.jon@gmail.com>
2013-06-04 09:27:11 +0200

    Added configuration models and LTW ontology model.

---
+++ src/eu/deustotech/internet/linkedtagworld/model/PropertyItem.java
@@ -0,0 +1,73 @@
+package eu.deustotech.internet.linkedtagworld.model;
+
+public class PropertyItem {
+	private String identifier = "";
+	private Boolean isLinkable = false;
+	private Boolean isMain = false;
+	private String ontologyProperty = "";
+	private String datatype = "";
+	private String language = "";
+	
+	public String getIdentifier() {
+		return identifier;
+	}
+	public Boolean getIsLinkable() {
+		return isLinkable;
+	}
+	public Boolean getIsMain() {
+		return isMain;
+	}
+	public String getOntologyProperty() {
+		return ontologyProperty;
+	}
+	public String getDatatype() {
+		return datatype;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+	public void setIsLinkable(Boolean isLinkable) {
+		this.isLinkable = isLinkable;
+	}
+	public void setIsMain(Boolean isMain) {
+		this.isMain = isMain;
+	}
+	public void setOntologyProperty(String ontologyProperty) {
+		this.ontologyProperty = ontologyProperty;
+	}
+	public void setDatatype(String datatype) {
+		this.datatype = datatype;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+	public boolean validate() {
+		if (this.identifier == "" || this.ontologyProperty == "") {
+			return false;
+		}
+		return true;
+	}
+	
+	@Override 
+	public String toString() {
+		StringBuilder result = new StringBuilder();
+	    String NEW_LINE = System.getProperty("line.separator");
+
+	    result.append(" 	 " + this.getClass().getName() + " Object {" + NEW_LINE);
+	    result.append("		  Identifier: " + identifier + NEW_LINE);
+	    result.append(" 	  Ontology Property: " + ontologyProperty + NEW_LINE);
+	    result.append(" 	  Linkable: " + isLinkable + NEW_LINE);
+	    result.append(" 	  Main: " + isMain + NEW_LINE);
+	    if (datatype != "")
+	    	result.append(" 	  Datatype: " + datatype + NEW_LINE);
+	    if (language != "")
+	    	result.append(" 	  Language: " + language + NEW_LINE);
+	    
+		return result.toString();		
+	}
+	
+}
