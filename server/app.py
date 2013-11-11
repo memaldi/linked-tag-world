54d5adb51b969d831929d43779af80cebf771776
Jon Lazaro <lazaro.jon@gmail.com>
2013-11-11 21:40:53 +0100

    Added style stuff and first views and templates of the website

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@
     :copyright: (c) 2013 by Jon Lazaro.
 """
 
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask import Flask, request, redirect, url_for, render_template
 
 import os
 
@@ -19,11 +19,30 @@ app = Flask(__name__)
 
 # Controllers
 @app.route("/")
-def hello():
-    return "Hello from Python!"
+def index():
+    return redirect(url_for('configure'))
+
+@app.route("/configure/")
+def configure():
+    return render_template('step0.html')
+
+@app.route("/configure/rdfsource/", methods=['GET', 'POST'])
+def rdfsource():
+    if request.method == 'POST':
+        app.logger.debug(request.form)
+        return render_template('rdf_step1.html')
+    else:
+        return render_template('rdf_step1.html')
+
+@app.route("/configure/nonrdfsource/", methods=['GET', 'POST'])
+def nonrdfsource():
+    if request.method == 'POST':
+        return render_template('nonrdf_step1.html')
+    else:
+        return render_template('nonrdf_step1.html')
 
 
 # Server launch
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
