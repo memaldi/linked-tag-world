ce73ffc8494c7beb739d324a5fb54e6b997a754b
Jon Lazaro <lazaro.jon@gmail.com>
2013-11-13 11:22:47 +0100

    Added RDF format select field in step 1

--- ltwserver/ltwserver/forms.py
+++ ltwserver/ltwserver/forms.py
@@ -1,15 +1,29 @@
 from flask_wtf import Form
 from wtforms.fields.html5 import URLField
-from wtforms.fields import TextAreaField
+from wtforms.fields import TextAreaField, TextField, SelectField
 from wtforms.validators import DataRequired, url
 from flask_wtf.file import FileField
 
+SUPPORTED_RDF_SYNTAXES = {
+    'xml': 'RDF/XML',
+    'turtle': 'Turtle',
+    'n3': 'Notation3 (N3)',
+    'nt': 'N-Triple',
+    'nquads': 'N-Quads',
+    'microdata': 'Microdata',
+    'rdfa': 'RDFa',
+    'rdfa1.0': 'RDFa 1.0',
+    'rdfa1.1': 'RDFa 1.1',
+    'trix': 'TriX',
+}
 
 class RDFDataForm(Form):
-    rdf_file = FileField('RDF file')
+    rdf_file = FileField('RDF file', validators=[DataRequired()])
+    format = SelectField(u'Format', choices=[ (key, value) for key, value in SUPPORTED_RDF_SYNTAXES.items() ])
 
 class SparqlForm(Form):
-    url = URLField('Endpoint', validators=[url()])
+    url = URLField('URL', validators=[url(), DataRequired()])
+    graph = TextField('Graph')
 
 class TermListForm(Form):
 	term_list = TextAreaField('Terms', validators=[DataRequired()])
