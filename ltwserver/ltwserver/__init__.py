f6ab9705c3cdd77589efee530a36f9cada0bea9d
Jon Lazaro <lazaro.jon@gmail.com>
2013-11-13 16:32:00 +0100

    Added result backend to celery

--- ltwserver/ltwserver/__init__.py
+++ ltwserver/ltwserver/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from celery import Celery
 
 def make_celery(app):
-    celery = Celery('hola', broker=app.config['CELERY_BROKER_URL'])
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'], broker=app.config['CELERY_BROKER_URL'])
     celery.conf.update(app.config)
 
     TaskBase = celery.Task
--- ltwserver/ltwserver/settings.py
+++ ltwserver/ltwserver/settings.py
@@ -1,5 +1,10 @@
 SECRET_KEY='\x05l%\xfeY3\xb2\xf9>\xba\xd9\xbfR\x82\xa3\xfd*lX>\x8a\x11\x98\xce'
 CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672/'
-#CELERY_RESULT_BACKEND = 'amqp://'
+#CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost:5672/'
+#CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_TASK_SERIALIZER='json'
+CELERY_RESULT_SERIALIZER='json'
+#CELERY_TASK_RESULT_EXPIRES = 18000
 CELERY_TIMEZONE = 'Europe/Madrid'
 CELERY_ENABLE_UTC = True
