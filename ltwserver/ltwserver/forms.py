b02dff92e9fd47a0b1ca53e47d8c4688774f0265
Jon Lazaro <lazaro.jon@gmail.com>
2013-11-13 16:29:38 +0100

    Added SPARQL URL validator

--- ltwserver/ltwserver/forms.py
+++ ltwserver/ltwserver/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import Form
 from wtforms.fields.html5 import URLField
 from wtforms.fields import TextAreaField, TextField, SelectField
-from wtforms.validators import DataRequired, url
+from wtforms.validators import DataRequired, url, ValidationError
 from flask_wtf.file import FileField
+from rdflib import ConjunctiveGraph
+
 
 SUPPORTED_RDF_SYNTAXES = {
     'xml': 'RDF/XML',
@@ -17,13 +19,22 @@ SUPPORTED_RDF_SYNTAXES = {
     'trix': 'TriX',
 }
 
+
+def validate_sparql_endpoint(form, field):
+    try:
+        g = ConjunctiveGraph('SPARQLStore')
+        g.open(field.data)
+        g.query('SELECT * WHERE { ?s ?p ?o } LIMIT 1')
+    except:
+        raise ValidationError('This is not a valid SPARQL endpoint.')
+
 class RDFDataForm(Form):
     rdf_file = FileField('RDF file', validators=[DataRequired()])
     format = SelectField(u'Format', choices=[ (key, value) for key, value in SUPPORTED_RDF_SYNTAXES.items() ])
 
 class SparqlForm(Form):
-    url = URLField('URL', validators=[url(), DataRequired()])
+    url = URLField('URL', validators=[url(), DataRequired(), validate_sparql_endpoint])
     graph = TextField('Graph')
 
 class TermListForm(Form):
-	term_list = TextAreaField('Terms', validators=[DataRequired()])
+    term_list = TextAreaField('Terms', validators=[DataRequired()])
