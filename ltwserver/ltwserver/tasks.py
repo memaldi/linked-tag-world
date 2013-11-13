1abbdc1d5c375d1f91909657e3f394757b8e682d
Jon Lazaro <lazaro.jon@gmail.com>
2013-11-13 16:33:42 +0100

    First steps into real-time notification with AJAX based polling + celery result backend

--- ltwserver/ltwserver/tasks.py
+++ ltwserver/ltwserver/tasks.py
@@ -2,6 +2,26 @@
 
 from ltwserver import celery
 
-@celery.Task
-def validate_sparql_endpoint():
-    pass
+from celery import current_task
+
+@celery.task()
+def generate_config_file(data_source, rdf_data=None, rdf_format=None, sparql_url=None, sparql_graph=None):
+    from time import sleep
+    current_task.update_state(state='PROGRESS', meta={'progress_percent': 20, 'progress_msg': 'Peeling potatoes...'})
+    print 'Peeling potatoes...'
+    sleep(3)
+    current_task.update_state(state='PROGRESS', meta={'progress_percent': 50, 'progress_msg': 'Frying potatoes...'})
+    print 'Frying potatoes...'
+    sleep(5)
+    current_task.update_state(state='PROGRESS', meta={'progress_percent': 55, 'progress_msg': 'Adding onion...'})
+    print 'Adding onion...'
+    sleep(2)
+    current_task.update_state(state='PROGRESS', meta={'progress_percent': 60, 'progress_msg': 'Stirring eggs...'})
+    print 'Stirring eggs...'
+    sleep(3)
+    current_task.update_state(state='PROGRESS', meta={'progress_percent': 75, 'progress_msg': 'Mixing all...'})
+    print 'Mixing all...'
+    sleep(3)
+    current_task.update_state(state='PROGRESS', meta={'progress_percent': 90, 'progress_msg': 'Doing the homellette...'}) 
+    print 'Doing the homellette...'
+    sleep(2)
--- ltwserver/ltwserver/views.py
+++ ltwserver/ltwserver/views.py
@@ -9,9 +9,13 @@
     :copyright: (c) 2013 by Jon Lazaro.
 """
 
-from ltwserver import app
+from ltwserver import app, celery
 from flask import request, redirect, url_for, render_template
 from ltwserver.forms import TermListForm, RDFDataForm, SparqlForm
+from ltwserver.tasks import generate_config_file
+from celery.result import AsyncResult
+
+import json
 
 @app.route("/")
 def index():
@@ -21,6 +25,16 @@ def index():
 def configure():
     return render_template('step0.html')
 
+@app.route('/_get_task_status')
+def get_task_status():
+    task_id = request.args.get('task_id', None, type=str)
+    if task_id:
+        task = AsyncResult(app=celery, id=task_id)
+        data = task.result or task.state
+    else:
+        data = {'error': 'No task_id in the request'}
+    return json.dumps(data)
+
 @app.route("/configure/rdfsource/", methods=['GET', 'POST'])
 def rdfsource():
     rdf_form = RDFDataForm(prefix='rdf')
@@ -28,9 +42,12 @@ def rdfsource():
     
     if request.method == 'POST' and len(request.form.keys()) > 0:
         if request.form.keys()[0].startswith('rdf-') and rdf_form.validate_on_submit():
-            app.logger.debug('RDF!')
+            t = generate_config_file.delay(data_source='rdf', rdf_data=request.files[rdf_form.rdf_file.name].read(), rdf_format=rdf_form.format.data)
+            return render_template('rdf_step1_a.html', data_source='rdf', task_id=t.task_id)
+
         elif request.form.keys()[0].startswith('sparql-') and sparql_form.validate_on_submit():
-            app.logger.debug('SPARQL!')
+            t = generate_config_file.delay(data_source='sparql', sparql_url=sparql_form.url.data, sparql_graph=sparql_form.graph.data)
+            return render_template('rdf_step1_a.html', data_source='sparql', task_id=t.task_id)
     
     return render_template('rdf_step1.html', rdf_form=rdf_form, sparql_form=sparql_form)
 
