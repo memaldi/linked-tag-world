27d99ed04f4edeb05e302153783aa849bafc074a
Jon Lazaro <lazaro.jon@gmail.com>
2013-11-19 15:42:55 +0100

    Added real functionality to Data fetching task

--- ltwserver/ltwserver/tasks.py
+++ ltwserver/ltwserver/tasks.py
@@ -120,22 +120,113 @@ def generate_config_file(data_source, rdf_data=None, rdf_format=None, sparql_url
 
 @celery.task()
 def get_all_data(data_source, config_file, rdf_data=None, rdf_format=None, sparql_url=None, sparql_graph=None):
-    from time import sleep
-    current_task.update_state(state='PROGRESS', meta={'progress_percent': 20, 'progress_msg': 'Peeling potatoes...'})
-    print 'Peeling potatoes...'
-    sleep(3)
-    current_task.update_state(state='PROGRESS', meta={'progress_percent': 50, 'progress_msg': 'Frying potatoes...'})
-    print 'Frying potatoes...'
-    sleep(5)
-    current_task.update_state(state='PROGRESS', meta={'progress_percent': 55, 'progress_msg': 'Adding onion...'})
-    print 'Adding onion...'
-    sleep(2)
-    current_task.update_state(state='PROGRESS', meta={'progress_percent': 60, 'progress_msg': 'Stirring eggs...'})
-    print 'Stirring eggs...'
-    sleep(3)
-    current_task.update_state(state='PROGRESS', meta={'progress_percent': 75, 'progress_msg': 'Mixing all...'})
-    print 'Mixing all...'
-    sleep(3)
-    current_task.update_state(state='PROGRESS', meta={'progress_percent': 90, 'progress_msg': 'Doing the omelette...'})
-    print 'Doing the omelette...'
-    sleep(2)
+    current_task.update_state(state='PROGRESS', meta={'progress_percent': 15, 'progress_msg': 'Reading RDF data...'})
+
+    if data_source == 'rdf':
+        data_graph = Graph()
+        data_graph.parse(format=rdf_format, data=rdf_data)
+    else:
+        data_graph = ConjunctiveGraph('SPARQLStore')
+        data_graph.open(sparql_url)
+
+    config_graph = Graph()
+    config_graph.parse(data=config_file, format='turtle')
+
+    data_dict = {}
+
+    config_q_res = config_graph.query(
+        """
+            SELECT DISTINCT ?id ?class_ont
+                WHERE {
+                    ?s a <http://helheim.deusto.es/ltw/0.1#ClassItem> ;
+                        <http://helheim.deusto.es/ltw/0.1#ontologyClass> ?class_ont ;
+                        <http://helheim.deusto.es/ltw/0.1#identifier> ?id .
+               }
+        """
+    )
+
+    current_task.update_state(state='PROGRESS', meta={'progress_percent': 30, 'progress_msg': 'Fetching all the data...'})
+
+    ont_class_tot = len(config_q_res)
+
+
+    for i, (class_id, class_ont) in enumerate(config_q_res):
+        current_task.update_state(state='PROGRESS', meta={'progress_percent': int(30 + (70 * float(i) / float(ont_class_tot))), 'progress_msg': 'Fetching %ss' % class_id.lower() })
+
+        data_dict[class_ont] = {}
+
+        graph_str = 'GRAPH <%s>' % sparql_graph if sparql_graph else ''
+
+        data_q_res = data_graph.query(
+        '''
+            SELECT DISTINCT ?s ?p ?o where {
+                %s
+                {
+                    ?s a <%s> ;
+                        ?p ?o .
+                }
+            }
+        ''' % (graph_str, class_ont)
+        )
+
+        main_props = get_main_props_by_class_ont(class_ont, config_graph)
+        linkable_props = get_linkable_props_by_class_ont(class_ont, config_graph)
+        props = get_props_by_class_ont(class_ont, config_graph)
+
+        for s, p, o in data_q_res:
+            if not s in data_dict[class_ont]:
+                data_dict[class_ont][s] = []
+
+            if str(p) in props:
+                data_dict[class_ont][s].append( (p, o, str(p) in main_props, str(p) in linkable_props) )
+
+    return data_dict
+
+def get_props_by_class_ont(class_ont, config_graph):
+    props = config_graph.query(
+    '''
+        SELECT DISTINCT ?prop_ont where {
+            ?s <http://helheim.deusto.es/ltw/0.1#ontologyClass> <%s> ;
+                <http://helheim.deusto.es/ltw/0.1#hasPropertyItem> ?prop .
+            ?prop <http://helheim.deusto.es/ltw/0.1#ontologyProperty> ?prop_ont .
+        }
+    ''' % class_ont
+    )
+    ret_list = []
+    for prop in props:
+        ret_list.append(str(prop[0]))
+    return ret_list
+
+def get_linkable_props_by_class_ont(class_ont, config_graph):
+    linkable_props = config_graph.query(
+    '''
+        SELECT DISTINCT ?prop_ont where {
+            ?s <http://helheim.deusto.es/ltw/0.1#ontologyClass> <%s> ;
+                <http://helheim.deusto.es/ltw/0.1#hasPropertyItem> ?prop .
+            ?prop <http://helheim.deusto.es/ltw/0.1#ontologyProperty> ?prop_ont ;
+                <http://helheim.deusto.es/ltw/0.1#isLinkable> ?linkable .
+            FILTER (?linkable)
+        }
+    ''' % class_ont
+    )
+    ret_list = []
+    for prop in linkable_props:
+        ret_list.append(str(prop[0]))
+    return ret_list
+
+def get_main_props_by_class_ont(class_ont, config_graph):
+    main_props = config_graph.query(
+    '''
+        SELECT DISTINCT ?prop_ont where {
+            ?s <http://helheim.deusto.es/ltw/0.1#ontologyClass> <%s> ;
+                <http://helheim.deusto.es/ltw/0.1#hasPropertyItem> ?prop .
+            ?prop <http://helheim.deusto.es/ltw/0.1#ontologyProperty> ?prop_ont ;
+                <http://helheim.deusto.es/ltw/0.1#isMain> ?main .
+            FILTER (?main)
+        }
+    ''' % class_ont
+    )
+    ret_list = []
+    for prop in main_props:
+        ret_list.append(str(prop[0]))
+    return ret_list
